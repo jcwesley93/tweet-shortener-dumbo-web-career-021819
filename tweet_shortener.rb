63936503d2a49f9c027ba4800cfcf7daefbbc730
Jaela Wesley <jcwesley93@gmail.com>
2019-02-12 15:38:15 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -31,3 +31,10 @@ def bulk_tweet_shortener(array)
     puts word_substituter(tweet)
     end
 end
+
+def selective_tweet_shortener(tweet)
+  if tweet.length > 140
+    word_substituter(tweet)
+  else return tweet
+  end
+end
